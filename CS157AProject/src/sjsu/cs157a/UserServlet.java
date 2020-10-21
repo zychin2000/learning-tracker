914a3e4260735c26099f153ad9ab852144465fea
Bellawei0 <56612745+Bellawei0@users.noreply.github.com>
2020-10-21 14:07:04 -0700

    Created login page and retrieved data from database

---
+++ CS157AProject/src/sjsu/cs157a/UserServlet.java
@@ -0,0 +1,67 @@
+package sjsu.cs157a;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/register")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	private UserJdbc UserDao;
+
+	public void init() {
+
+		UserDao = new UserJdbc();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String studentID = request.getParameter("StudentID");
+
+		String firstName = request.getParameter("firstName");
+
+		String lastName = request.getParameter("lastName");
+
+		String phone = request.getParameter("phone");
+
+		String email = request.getParameter("email");
+
+		String password = request.getParameter("password");
+
+		User user = new User();
+
+		user.setStudentID(studentID);
+
+		user.setFirstName(firstName);
+
+		user.setLastName(lastName);
+
+		user.setPhoneNo(phone);
+
+		user.setEmail(email);
+
+		user.setPassword(password);
+
+		try {
+
+			UserDao.registerUser(user);
+
+		} catch (Exception e) {
+
+			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+
+		}
+
+		response.sendRedirect("details.jsp");
+
+	}
+
+}
