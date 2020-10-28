39a0c1449b615f9ae14f954ce0befb5890b6c1a4
zychin2000 <zychin2000@gmail.com>
2020-10-28 21:11:10 +0800

    Changed the project from HTML project to Jave EE/JSP Project

---
+++ src/main/java/com/learningprinciples/model/LiquorType.java
@@ -0,0 +1,9 @@
+package com.learningprinciples.model;
+
+/**
+ * Created by kasun on 5/24/17.
+ */
+public enum LiquorType {
+    WINE,BEER,WHISKY
+
+}
---
+++ src/main/java/com/learningprinciples/model/Student.java
@@ -0,0 +1,27 @@
+package com.learningprinciples.model;
+
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Student(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    // constructors, getters and setters goes here
+}
