b42957c90e0f88c18b4d862c58ca1f0bbc810297
Bellawei0 <56612745+Bellawei0@users.noreply.github.com>
2020-11-12 21:41:43 -0800

    added DeleteNoteServlet for deletion functionlity

---
+++ CS157AProject/src/sjsu/cs157a/servlets/DeleteNoteServlet.java
@@ -0,0 +1,43 @@
+package sjsu.cs157a.servlets;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import sjsu.cs157a.config.DatabaseConnection;
+import sjsu.cs157a.dao.NoteDAO;
+
+@WebServlet("/dashboard/delete")
+public class DeleteNoteServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private static NoteDAO noteDao;
+
+	public void init() {
+		noteDao = new NoteDAO(new DatabaseConnection());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		int id = Integer.parseInt(request.getParameter("noteId"));
+		try {
+			noteDao.deleteNote(id);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		response.sendRedirect("notelist");
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
