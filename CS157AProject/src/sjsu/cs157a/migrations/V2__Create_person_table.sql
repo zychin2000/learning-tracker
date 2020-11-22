ee78d5f042654d2e4e2008bf421f235d197ffa07
Bellawei0 <56612745+Bellawei0@users.noreply.github.com>
2020-11-22 09:17:20 -0800

    added sql statement to drop princile_id

--- CS157AProject/src/sjsu/cs157a/migrations/V2__Create_person_table.sql
+++ CS157AProject/src/sjsu/cs157a/migrations/V2__Create_person_table.sql
@@ -1,3 +1,7 @@
 # change the length of password to 100
 ALTER TABLE `project157a`.`user_register`
     CHANGE COLUMN `password` `password` VARCHAR(100) NULL DEFAULT NULL ;
+    
+# Drop Column principle_id from note_meta
+ALTER TABLE `project157a`.`note_meta` 
+DROP COLUMN `principle_id`;
